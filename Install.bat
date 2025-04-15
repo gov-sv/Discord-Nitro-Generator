57d70f79c247b05c64ab0d740db305d68e2ca1ba
gov-sv <kakaki8462@gmail.com>
2025-04-15 23:08:31 +0300

    New Api Discord Nitro Generator
    New Api Updated Discord Nitro Generator

---
+++ Install.bat
@@ -0,0 +1,29 @@
+@echo off
+echo [31m[.] Installing Requirements[0m  
+timeout 2 >nul
+cls
+echo [31m[..] Installing Requirements[0m
+timeout 2 >nul
+cls
+echo [31m[...] Installing Requirements[0m
+timeout 2 >nul
+cls
+echo [31m[.] Installing Requirements[0m
+timeout 2 >nul
+cls
+echo [31m[..] Installing Requirements[0m
+timeout 2 >nul
+cls
+echo [31m[...] Installing Requirements[0m
+timeout 2 >nul
+cls
+pip install Clear-Screen
+pip install Colorama
+pip install Requests
+pip install sys
+pip install os
+pip install time
+pip install string
+Cls
+echo [32m[+] Succesfully Installed Requrements[0m
+pause
